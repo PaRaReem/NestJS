a45dc66cc86b6f609135c80bac4b738eea0aa681
Saeheng - EXT, Wuttipat <164000730+PaRaReem@users.noreply.github.com>
2025-01-23 17:58:56 +0700

    interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ResponseInterceptor } from './common/middlewares/response-format.interceptor';
-import { LoggerInterceptor } from './common/middlewares/logger.interceptor';
+import { ResponseInterceptor } from './common/interceptors/response-format.interceptor';
+import { LoggerInterceptor } from './common/interceptors/logger.interceptor';
 import { ConfigService } from '@nestjs/config';
 import { FirebaseService } from './modules/firebase/firebase.service';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/exceptions/http.exceptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(
+    new ResponseInterceptor(), //* แปลง response format
+    new LoggerInterceptor(), //* log request,response
+  );
   const configService = app.get(ConfigService);
   const port = configService.get('app.port');
   // Get FirebaseService and test the connection
@@ -17,10 +23,6 @@ async function bootstrap() {
   console.log(connectionStatus);
   //* ใช้งาน Interceptor ระดับ Global
   //* AsyncStorage ทำงานคล้ายๆ Redux
-  app.useGlobalInterceptors(
-    new ResponseInterceptor(), //* แปลง response format
-    new LoggerInterceptor(), //* log request,response
-  );
   app.enableCors();
   await app.listen(port ?? 8088);
   console.log(`server started on port ${port}`);
